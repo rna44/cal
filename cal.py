cb3ec1db8b1a875b169b179ea9c481a86017195b
rna44 <rinesabunjaku02@gmail.com>
2026-07-11 15:08:46 +0200

    Basic calculator

---
+++ cal.py
@@ -0,0 +1,20 @@
+num1 = float(input("Enter first number: "))
+operation = input("Choose operation (+, -, *, /)")
+num2 = float(input("Enter second number: "))
+
+if operation == "+":
+    result = num1 + num2
+
+elif operation == "-":
+    result = num1 - num2
+
+elif operation == "*":
+    result = num1 * num2
+
+elif operation == "/":
+    result = num1 / num2
+
+else:
+    result = "invalid operation"
+
+print("Result:", result)
